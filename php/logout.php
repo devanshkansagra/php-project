433f7e0ce9cddae7982599709b21e500f82d2f7b
Devansh Kansagra <22it423@bvmengineering.ac.in>
2024-04-23 18:29:56 +0530

    Implemented Logout functionality

--- php/logout.php
+++ php/logout.php
@@ -1,5 +1,7 @@
 <?php
     session_unset();
     session_destroy();
+    setcookie("Email", "", time() - 1, "/");
+    setcookie("Password", "", time() - 1, "/");
     header("Location: ../public/index.html");
 ?>
